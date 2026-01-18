9c44df13575a13a3efdf1e3dab9b88d977d3adf1
nico <yuuuuuuuu.13@gmail.com>
2026-01-19 06:59:08 +0900

    ルート作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AttendanceController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/attendance', [AttendanceController::class, 'userAttendance'])->name('user.attendance'); //出勤登録画面（一般ユーザー）
+    Route::get('/attendance/list', [AttendanceController::class, 'userIndex'])->name('user.index'); //勤怠一覧画面（一般ユーザー）
+    Route::get('/stamp_correction_request/list', [AttendanceController::class, 'requestIndex'])->name('request.index'); //申請一覧画面（管理者）（一般ユーザー）
+    Route::get('/attendance/detail/{id}', [AttendanceController::class, 'userDetail'])->name('user.detail'); //勤怠詳細画面（一般ユーザー）
+});
+
+Route::middleware(['auth', 'verified', 'admin'])->group(function () {
+    Route::get('/admin/attendance/list', [AttendanceController::class, 'adminIndex'])->name('admin.index'); //勤怠一覧画面（管理者）
+    Route::get('/admin/attendance/{id}', [AttendanceController::class, 'adminDetail'])->name('admin.detail'); //勤怠詳細画面（管理者）
+    Route::get('/admin/staff/list', [AttendanceController::class, 'adminStaffIndex'])->name('admin.staff.index'); //スタッフ一覧画面（管理者）
+    Route::get('/admin/attendance/staff/{id}', [AttendanceController::class, 'adminStaffShow'])->name('admin.attendance.show'); //スタッフ別勤怠一覧画面（管理者）
+    Route::get('/stamp_correction_request/approve/{attendance_correct_request_id}', [AttendanceController::class, 'adminRequestShow'])->name('admin.request.show'); //修正申請承認画面（管理者）
 });
